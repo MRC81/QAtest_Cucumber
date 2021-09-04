7b0d069266fd53031c53ff87e0fb3c57492c093c
Serge <dexmach@gmail.com>
2021-09-04 20:56:52 +0300

    Updated build.gradle, SelenideConfiguration and CucumberRunner

--- src/test/java/cucumber/glue/hooks/SelenideConfiguration.java
+++ src/test/java/cucumber/glue/hooks/SelenideConfiguration.java
@@ -2,10 +2,6 @@ package cucumber.glue.hooks;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.junit5.TextReportExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
 import static java.lang.Boolean.parseBoolean;
 
 
@@ -13,18 +9,16 @@ public class SelenideConfiguration {
 
     public void getBrowser(){
         String browser = System.getProperty("selenide.browser", "chrome");
-//       boolean headless = parseBoolean(System.getProperty("selenide.headless", "false"));
+        boolean headless = parseBoolean(System.getProperty("selenide.headless", "false"));
 
         Configuration.browser = browser;
-        Configuration.headless = false;
+        Configuration.headless = headless;
         Configuration.startMaximized = true;
         Configuration.screenshots = true;
+        Configuration.savePageSource =false;
         Configuration.reportsFolder = "reports";
 
         Configuration.baseUrl = "https://www.investing.com";
-
-        clearBrowserCache();
-        WebDriverRunner.driver().clearCookies();
     }
 
     public void closeSession(){
