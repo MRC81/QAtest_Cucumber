608493be0e907f14951552bf53a0ad9218e7d38a
sergeib-investing <sergei.b@investing.com>
2023-07-08 20:47:39 +0200

    Allure added

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -5,10 +5,16 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = {"cucumber.glue.hooks", "cucumber.glue.steps"},
-        plugin = {"pretty", "html:reports/cucumber-report.html"},
+        plugin = {
+//                "progress",
+//                "summary",
+//                "pretty",
+                "html:reports/cucumber-report.html",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
         features = {"src/test/resources/features"},
-        monochrome =true,
-        publish = true
+        monochrome =false,
+        publish = false
 )
 
 public class CucumberRunner {
